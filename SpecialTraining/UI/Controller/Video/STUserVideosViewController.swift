bc9b60ac01228e1d0c4e6541dae51f124193ee4f
mlsy_swift <>
2018-12-03 21:42:55 +0800

    点播二级界面

--- SpecialTraining/UI/Controller/Video/STUserVideosViewController.swift
+++ SpecialTraining/UI/Controller/Video/STUserVideosViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class STUserVideosViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var scroll: UIScrollView!
+    @IBOutlet weak var bgHeader: UIView!
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var topViewHeightCns: NSLayoutConstraint!
+    @IBOutlet weak var topSaveAreaCns: NSLayoutConstraint!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func setupUI() {
+        if UIDevice.current.isX == true {
+            topViewHeightCns.constant = 130 + 44
+            topSaveAreaCns.constant = 20 + 44
+        }else {
+            topViewHeightCns.constant = 130
+            topSaveAreaCns.constant = 20
+        }
+        
+        let frame = CGRect.init(x: 0, y: 0, width: PPScreenW, height: topViewHeightCns.constant)
+        bgHeader.layer.insertSublayer(STHelper.themeColorLayer(frame: frame), at: 0)
     }
     
+    override func rxBind() {
+        
+    }
+
 }
