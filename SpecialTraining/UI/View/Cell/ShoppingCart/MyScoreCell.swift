08d902521b4f0edf029b4d5f07bfa8c52d97345f
mlsy_swift <>
2018-12-13 18:25:11 +0800

    阳光分数

---
+++ SpecialTraining/UI/View/Cell/ShoppingCart/MyScoreCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyScoreCell.swift
+//  SpecialTraining
+//
+//  Created by sw on 13/12/2018.
+//  Copyright © 2018 youpeixun. All rights reserved.
+//
+
+import UIKit
+
+class MyScoreCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
