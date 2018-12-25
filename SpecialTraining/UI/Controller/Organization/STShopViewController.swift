76bc4589648aea255df27ee2ab97163b1154c4ac
mlsy_swift <>
2018-12-25 21:41:59 +0800

    ffff

---
+++ SpecialTraining/UI/Controller/Organization/STShopViewController.swift
@@ -0,0 +1,30 @@
+//
+//  STShopViewController.swift
+//  SpecialTraining
+//
+//  Created by sw on 25/12/2018.
+//  Copyright © 2018 youpeixun. All rights reserved.
+//
+
+import UIKit
+
+class STShopViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
