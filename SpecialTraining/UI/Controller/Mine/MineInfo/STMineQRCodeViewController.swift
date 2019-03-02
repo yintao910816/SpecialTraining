a6e2d384371e96151e8a9b853ea36ab628386f62
yintao910816 <ytappledeveloper@163.com>
2019-03-03 02:04:35 +0800

    我的二维码

--- SpecialTraining/UI/Controller/Mine/MineInfo/STMineQRCodeViewController.swift
+++ SpecialTraining/UI/Controller/Mine/MineInfo/STMineQRCodeViewController.swift
@@ -13,10 +13,9 @@ import RxCocoa
 class STMineQRCodeViewController: BaseViewController {
 
     @IBOutlet weak var ivQRCode: UIImageView!
-    @IBOutlet weak var ivHead: UIImageView!
     
     override func setupUI() {
-        title = "我的二维码"
+
     }
     
     override func rxBind() {
@@ -24,19 +23,8 @@ class STMineQRCodeViewController: BaseViewController {
     }
     
     @IBAction func longPressAction(_ sender: UILongPressGestureRecognizer) {
-        PrintLog("长按图片")
-        if (sender.state == UIGestureRecognizer.State.began) {
-            //1.初始化扫描仪，设置识别类型和识别质量
-            let options = ["IDetectorAccuracy":CIDetectorAccuracyHigh]
-            let detector:CIDetector = CIDetector(ofType: "CIDetectorTypeQRCode", context: nil, options: options)!
-            //2.扫描获取的特征组
-            let features = detector.features(in: CIImage(cgImage: (self.ivQRCode.image?.cgImage)!))
-            //3.获取扫描结果
-            if features.count > 0 {
-                let feature = features[0] as! CIQRCodeFeature
-                let scanResult = feature.messageString
-                PrintLog(scanResult)
-            }
+        NoticesCenter.alertActionSheet(actionTitles: ["保存"], cancleTitle: "取消",presentCtrl: self) { idx in
+            PrintLog(idx)
         }
     }
     
