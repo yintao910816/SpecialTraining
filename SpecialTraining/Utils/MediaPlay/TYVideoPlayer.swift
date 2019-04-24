1c326d5b8c88fa71a0a229de73cccc69cb638fc7
yintao910816 <ytappledeveloper@163.com>
2019-04-25 02:41:21 +0800

    视屏播放进度条，还有bug

--- SpecialTraining/Utils/MediaPlay/TYVideoPlayer.swift
+++ SpecialTraining/Utils/MediaPlay/TYVideoPlayer.swift
@@ -15,6 +15,15 @@ class TYVideoPlayer {
     private var urlString: String = ""
     private var playView: UIView!
     
+    private var bgProgress: UIView!
+    // 缓冲进度
+    private var prepareProgress: UIView!
+    // 播放进度
+    private var playProgress: UIView!
+    
+    private var totleTime: TimeInterval = 1.0
+    private var timer: CountdownTimer!
+
     private var player: AVPlayer!
     
     private let disposeBag = DisposeBag()
@@ -35,7 +44,51 @@ class TYVideoPlayer {
         urlString = url
         self.playView = playView
         
+        setupView()
         configPlay()
+    }
+    
+    private func setupView() {
+        bgProgress = UIView()
+        bgProgress.backgroundColor = .red//RGB(220, 220, 220)
+        
+        prepareProgress = UIView()
+        prepareProgress.backgroundColor = .white
+        
+        playProgress = UIView()
+        playProgress.backgroundColor = RGB(243, 153, 52)
+        
+        playView.addSubview(bgProgress)
+        bgProgress.addSubview(prepareProgress)
+        bgProgress.addSubview(playProgress)
+        
+        playView.bringSubviewToFront(bgProgress)
+        
+        var bottom: CGFloat = 0
+        if #available(iOS 11, *) {
+            if let w = UIApplication.shared.delegate?.window, let unwrapedWindow = w {
+                bottom = unwrapedWindow.safeAreaInsets.bottom
+            }
+        }
+        
+        bgProgress.snp.makeConstraints {
+            $0.left.equalTo(0)
+            $0.width.equalTo(playView.width)
+            $0.height.equalTo(1)
+            $0.bottom.equalTo(-bottom)
+        }
+        
+        prepareProgress.snp.makeConstraints {
+            $0.left.bottom.equalTo(0)
+            $0.width.equalTo(playView.width)
+            $0.height.equalTo(bgProgress.snp.height)
+        }
+        
+        playProgress.snp.makeConstraints {
+            $0.left.bottom.equalTo(0)
+            $0.width.equalTo(playView.width)
+            $0.height.equalTo(bgProgress.snp.height)
+        }
     }
     
     private func configPlay() {
@@ -62,6 +115,12 @@ class TYVideoPlayer {
                 let loadedTime = strongSelf.avalableDurationWithplayerItem()
                 let totalTime = CMTimeGetSeconds(playerItem.duration)
                 let percent: CGFloat = CGFloat(loadedTime/totalTime)
+                if totalTime > 0 {
+                    strongSelf.totleTime = totalTime
+                    PrintLog("video totleTime - \(totalTime)")
+                    let pregress: CGFloat = strongSelf.playView.width * percent
+                    strongSelf.prepareProgress.snp.updateConstraints{ $0.width.equalTo(pregress) }
+                }
                 PrintLog("视屏缓冲进度 -- \(percent)")
                 return percent
             })
@@ -80,12 +139,30 @@ class TYVideoPlayer {
         urlString = url
         self.playView = playView
         
+        if bgProgress == nil {
+            setupView()
+        }
+        
         configPlay()
     }
     
     public func play() {
         if player.rate == 0 {
+            if timer == nil {
+                let totleInt: Int = Int(round(totleTime))
+                timer = CountdownTimer.init(timeInterval: 1, totleCount: totleInt)
+                timer.countTime.asDriver()
+                    .drive(onNext: { [weak self] count in
+                        guard let strongSelf = self else { return }
+                        let percent: CGFloat = CGFloat(count/totleInt)
+                        let pregress: CGFloat = strongSelf.playView.width * percent
+                        strongSelf.playProgress.snp.updateConstraints{ $0.width.equalTo(pregress) }
+                    })
+                    .disposed(by: disposeBag)
+            }
+
             player.play()
+            timer.timerStar()
         }
     }
     
